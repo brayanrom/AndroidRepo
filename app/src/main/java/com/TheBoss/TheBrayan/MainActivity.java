c10fc03d5013d328ed3acb810e67c442219e06e5
brayan <brayan_itai@hotmail.com>
2020-09-23 11:25:52 -0500

    Primera subida de todo

---
+++ app/src/main/java/com/TheBoss/TheBrayan/MainActivity.java
@@ -0,0 +1,19 @@
+package com.TheBoss.TheBrayan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+
+}
